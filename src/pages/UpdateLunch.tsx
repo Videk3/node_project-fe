4b058c34e748f8b1f5c3e0fd84c7d3e7880da047
Videk3 <vid.berlocnik2@scv.si>
2023-06-29 15:59:52 +0200

    Added page for updating lunch name

---
+++ src/pages/UpdateLunch.tsx
@@ -0,0 +1,58 @@
+import './Register.css';
+import {FC, SyntheticEvent, useState} from "react";
+import axios from "axios";
+import {Navigate} from "react-router-dom";
+
+interface Props {
+    id: number;
+}
+
+const UpdateLunch:FC<Props> = ({id}) => {
+
+    const[name, setName] = useState('');
+
+    const[redirect, setRedirect] = useState(false);
+    const[errorText, setErrorText] = useState('');
+
+    const submit = async (e: SyntheticEvent) => {
+        e.preventDefault();
+
+        const data = {
+            name,
+        };
+        console.log(data);
+        const res = await axios.patch(`http://localhost:3000/lunches/${id}`, data, {withCredentials: true});
+        console.log(res);
+
+        if(res.status == 201) {
+            setRedirect(true);
+        }
+        if(res.status != 201){
+            setErrorText('Napaka v podatkih');
+        }
+    }
+
+    if(redirect) {
+        return <Navigate to={'/'}/>;
+    }
+
+    return(
+        <>
+            <main className="form-signin w-100 m-auto">
+                <h2 className={"error"}>{errorText}</h2>
+                <form onSubmit={submit}>
+                    <h1 className="h3 mb-3 fw-normal">Update lunch</h1>
+                    <div className="form-floating">
+                        <input type="text" className="form-control" id="floatingInput"
+                               placeholder="Updated lunch name"
+                               onChange={(e) => setName(e.target.value)} />
+                        <label htmlFor="floatingInput">Name</label>
+                    </div>
+                    <button className="w-100 btn btn-lg btn-primary" type="submit">Update</button>
+                </form>
+            </main>
+        </>
+    )
+}
+
+export default UpdateLunch;
